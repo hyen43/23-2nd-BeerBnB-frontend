3ca086174e4b52bcafa21069f5d5b826f2c71729
Kayoungkimjs <kykim.dev@gmail.com>
2021-08-19 17:33:40 +0900

    MODIFY: 소셜 로그인 최종 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Detail from '../src/pages/Detail/Detail';
-import Nav from '../src/components/Nav/Nav';
-import Footer from '../src/components/Footer/Footer';
+// import Nav from '../src/components/Nav/Nav';
+// import Footer from '../src/components/Footer/Footer';
+import Login from './pages/Login/Login';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
-        <Nav />
+        {/* <Nav /> */}
         <Switch>
-          {/* <Route exact path="/" component={Main} /> */}
           <Route exact path="/detail" component={Detail} />
+          <Route exact path="/login" component={Login} />
         </Switch>
-        <Footer />
+        {/* <Footer /> */}
       </Router>
     );
   }
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const BASE_URL = 'http://10.58.5.125:8000';
+
+export const API = {
+  LOGIN: `${BASE_URL}/users/signin`,
+};
+
+export const TOKEN_KEY = 'TOKEN';
